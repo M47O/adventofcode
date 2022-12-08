9e9cc4114b52c44805931e041e0f96a4c3b02eda
Mason Turner <turner_m@lynchburg.edu>
2022-12-08 08:12:05 -0500

    Complete Part Two

--- 2022/day1/main.js
+++ 2022/day1/main.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 
-// Part One:
+
 let input = fs.readFileSync(__dirname + '/input.txt', 'utf8')
 let elves = input.split('\n\n')                         // Splits the input into an array, separating each element whenever there's a double new line ('\n\n')
         .map(x => x.split('\n'))                        // Splits the array into an array of arrays, each subarray representing an elf
@@ -11,5 +11,16 @@ let totalCalories = elves.map(x => {                    // Iterates over the elv
     return x.reduce((acc,cv) => acc+cv,0)               // reducing each element (subarray) to one number by summing their numbers
 })
 
+
 //Part One Solution:
-return Math.max(...totalCalories)
+function mostCalories(){        
+    return Math.max(...totalCalories)
+}
+
+//Part Two Solution:
+function topThreeTotal(){
+    const topThree = totalCalories.sort((a,b) => a-b).slice(-3)     // Sort the array of the elves' calories totals in ascending order, put the last three in a variable (as an array)
+
+    return topThree.reduce((acc,cv) => acc+cv,0)                    // Get the sum of each array element, same as before
+
+}
